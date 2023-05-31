f64dc0d5c0690ef7a7e1138f19e52a84a2d55219
Methas Tariya <korstudio@gmail.com>
2023-05-31 20:06:33 +0900

    Initial project

---
+++ SiriusiOS/SiriusiOS/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  SiriusiOS
+//
+//  Created by Methas Tariya on 31/5/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
---
+++ SiriusiOS/SiriusiOS/ViewControllers/Home/HomeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewModel.swift
+//  SiriusiOS
+//
+//  Created by Methas Tariya on 31/5/23.
+//
+
+import Foundation
+import RxSwift
+
+protocol HomeViewModelProtocol {
+
+}
+
+final class HomeViewModel: HomeViewModelProtocol {
+
+}
